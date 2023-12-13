1da66482ca3c0ebb461fe38b592cd18c3e982676
allamypereira <allamympereira@gmail.com>
2023-12-13 14:35:04 -0300

    repositorio criado

---
+++ src/main/java/br/edu/ifpe/recife/gcms/webapp/HomeController.java
@@ -0,0 +1,22 @@
+package br.edu.ifpe.recife.gcms.webapp;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public @ResponseBody String greeting() {
+        return "Hello, World";
+    }
+
+    @RequestMapping("/niviane")
+    public @ResponseBody String greetingNiviane() {
+        return "Hello, Niviane!";
+    }
+
+
+}
