a4888eda33bd96df6f3417f060bfbaaf39e29427
Niviane <nivianecriativa@gmail.com>
2023-12-20 15:07:35 -0300

    commit do clone

--- src/main/java/br/edu/ifpe/recife/gcms/webapp/HomeController.java
+++ src/main/java/br/edu/ifpe/recife/gcms/webapp/HomeController.java
@@ -13,7 +13,7 @@ public class HomeController {
         return "Hello, World";
     }
 
-    @RequestMapping("/niviane2")
+    @RequestMapping("/niviane24")
     public @ResponseBody String greetingNiviane() {
         return "Hello, Niviane!";
     }
